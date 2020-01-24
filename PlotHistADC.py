1948985a2af6130ce3678d9304038059cd143030
岐部佳朗 <kibe@kibe-iMac.local>
2020-01-24 12:57:20 +0900

    Update PlotHistADC.py

--- PlotHistADC.py
+++ PlotHistADC.py
@@ -22,20 +22,36 @@ parser.add_argument('-cut',
                     dest = 'isCut',
                     help = 'draw histgram with selection criteria')
 
-parser.add_argument('-fit',
+parser.add_argument('-norm',
                     action = 'store_true',
-                    dest = 'isFit',
-                    help = 'fitting histgram')
+                    dest = 'isNorm',
+                    help = 'draw histgram with normalization')
+
+parser.add_argument('-fit',
+                    #action = 'store_true',
+                    dest = 'FitRange',
+                    nargs = 2,
+                    default = [],
+                    type = float,
+                    #dest = 'isFit',
+                    help = 'set fitting range')
 
 args = parser.parse_args()
 
 FileIN = args.FileIN
 isFigOut = args.isFigOut
 isCut = args.isCut
-isFit = args.isFit
+isNorm = args.isNorm
+FitRange = args.FitRange
 
 if(len(FileIN) <= 0):
     sys.exit()
+    pass
+
+isFit = False
+
+if(len(FitRange) > 0):
+    isFit = True
     pass
 
 tr = []
@@ -49,6 +65,13 @@ for i in range(len(FileIN)):
 for i in range(len(RFile)):
     tmp = RFile[i].Get('tr')
     tr.append(tmp)
+    pass
+
+SourceType = []
+
+for i in range(len(FileIN)):
+    tmp = re.findall('./(.*)/(.*)/(.*)_(.*).root', FileIN[i])
+    SourceType.append(tmp[0][-1])
     pass
 
 c1 = ROOT.TCanvas('c1', 'c1', 0, 0, 1067, 750)
@@ -65,9 +88,10 @@ c1.SetGridy()
 #c1.SetLogy()
 
 MinADC = 0.0
-MaxADC = numpy.array([tr[i].GetMaximum('ADC') for i in range(len(tr))]).min()
+#MaxADC = numpy.array([tr[i].GetMaximum('ADC') for i in range(len(tr))]).min()
+MaxADC = 200.0
 
-ADCBinWidth = 0.50
+ADCBinWidth = 0.2
 NofADCBin = (int)((MaxADC - MinADC)/ADCBinWidth)
 
 hADC = []
@@ -80,7 +104,7 @@ Duaration = []
 #CutValue = [4.8, 0.0] #PC1%, Cs
 #CutValue = [0.0, 58.0] #PC5%, Co
 #CutValue = [6.2, 0.0] #PC5%, Cs
-CutValue = [0.0, 62.0] #PC10%, Co
+CutValue = [6.8, 58.0] #PC10%, Co
 #CutValue = [0.0, 49.0] #PC10%, Cs
 #CutValue = [0.0, 60.0] #PC10%, Na
 #CutValue = [0.0, 65.0] #PC20%, Co
@@ -96,8 +120,10 @@ CutPGA = ROOT.TCut('(-PeakHeight)/(-AmpSample) > %2.2f' %(CutValue[0]))
 CutDWT = ROOT.TCut('Power2/Power1 > %2.2f' %(CutValue[1]))
 
 #fot fitting function        
-MinFit = 20.0
-MaxFit = 100.0
+if(isFit):
+    MinFit = numpy.min(numpy.array(FitRange))
+    MaxFit = numpy.max(numpy.array(FitRange))
+    pass
 
 fFit = [ROOT.TF1() for i in xrange(len(tr))]
 fErfc = [ROOT.TF1() for i in xrange(len(tr))]
@@ -109,7 +135,8 @@ for i in range(len(tr)):
     tmp_Cut = ROOT.TH1D('hADC_%d_Cut' %(i), 'ADC Total', NofADCBin, MinADC, MaxADC)
 
     tr[i].Draw('ADC>>hADC_%d' %(i), '', 'Q0')
-    tr[i].Draw('ADC>>hADC_%d_Cut' %(i), CutPGA + CutDWT, 'Q0')
+    #tr[i].Draw('ADC>>hADC_%d_Cut' %(i), CutDWT, 'Q0')
+    tr[i].Draw('ADC>>hADC_%d_Cut' %(i), CutPGA, 'Q0')
     
     hADC.append(tmp)
     hADC_Cut.append(tmp_Cut)
@@ -147,21 +174,26 @@ for i in range(len(hADC)):
     hADC_Cut[i].SetFillColor(4)
     hADC_Cut[i].SetFillStyle(3002)
     
-    #normalize
-    #Norm = hADC[i].GetEntries()
-    Norm = Duaration[i]
-
+    #calc event rate
     print '%s:' %(FileIN[i])
-    print 'Duaration time = %.3f sec' %(Norm)
-    print 'Event rate = %.3f Hz' %(hADC[i].GetEntries()/Norm)
-    print 'Event rate = %.3f Hz after noise reduction' %(hADC_Cut[i].GetEntries()/Norm)
-    
-    #hADC[i].Sumw2()
-    #hADC[i].Scale(1.0/Norm)
-    
-    #hADC_Cut[i].Sumw2()
-    #hADC_Cut[i].Scale(1.0/Norm)
+    print 'Duaration time = %.3f sec' %(Duaration[i])
+    print 'Event rate = %.3f Hz' %(hADC[i].GetEntries()/Duaration[i])
+    print 'Event rate = %.3f Hz after noise reduction' %(hADC_Cut[i].GetEntries()/Duaration[i])
 
+    #normalized by total events
+    Norm = hADC[i].Integral()
+
+    if(isNorm):
+        hADC[i].Sumw2()
+        hADC[i].Scale(1.0/Norm)
+    
+        #hADC_Cut[i].Sumw2()
+        #hADC_Cut[i].Scale(1.0/Norm)
+
+        pass
+
+    print 'Number of events = %d' %(Norm)
+    
     if(i != 0):
         hADC[i].Draw('histsame')
     else:
@@ -175,23 +207,22 @@ for i in range(len(hADC)):
 
     #fitting
     if(isFit):
-
+        
         fFit[i] = ROOT.TF1('fFit_%d' %(i), '[0]* TMath::Erf(-(x - [1])/[2]) + [3]', MinFit, MaxFit)
-
+        
         #initial values of fitting parameters
         Index = int((((MaxFit - MinFit)/2.0) - MinADC)/ADCBinWidth)
         Par0 = 0.5* hADC[i].GetMaximum()
         Par1 = (MaxFit - MinFit)/2.0
         Par2 = 1.0e+2
-        Par3 = hADC[i].GetBinContent(Index)
+        #Par3 = hADC[i].GetBinContent(Index)
+        Par3 = Par0/2.0
         
         fFit[i].SetParameter(0, Par0)
         fFit[i].SetParameter(1, Par1)
         fFit[i].SetParameter(2, Par2)
         fFit[i].SetParameter(3, Par3)
 
-        #fFit[i].SetParLimits(3, 0.0, 1.0e+6)
-        
         if(isCut):
             hADC_Cut[i].Fit('fFit_%d' %(i), 'R0')
         else:
@@ -234,7 +265,7 @@ c1.Update()
 
 #figure
 if(isFigOut):
-    FileName = re.findall('./Output/(.*)_(.*).root', FileIN[0])
+    FileName = re.findall('(.*)_(.*).root', FileIN[0])
     FigFile = './Figures/%s_ADC.gif' %(FileName[0][1])
     c1.Print(FigFile)
     pass
